a076a0b7a839291b896655b94083b0643b442178
GDash24 <136406487+GDash24@users.noreply.github.com>
2024-01-28 17:56:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log()
+f_name.concat(l_name)
+var first_name= "Ranbir";
+var last_name= "Kapoor";
+
+var a = 10;
+var b = 5;
+var add= a + b ;
+console.log(add)
+
+var subtract= a - b;
+console.log(subtract)
+
+var mutiply= a * b;
+console.log(multiply)
+
+var divide= a / b;
+console.log(divide)
